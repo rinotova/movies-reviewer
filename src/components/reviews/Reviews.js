b7bfbdf1b290d8587c093e3326987ce4f0c79c16
Rino Tovar <rinotova@gmail.com>
2022-07-28 10:50:57 +0200

    Comments

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -24,6 +24,7 @@ const Reviews = (props) => {
 
   const addedCommentHandler = useCallback(() => {
     sendRequest(movieId);
+    setIsAddingComment(false);
   }, [sendRequest, movieId]);
 
   let comments;
