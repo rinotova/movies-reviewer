1669b0324b7e3d29eda3078edd28f51888a6ef4c
Rino Tovar <rinotova@gmail.com>
2022-07-29 12:51:10 +0200

    Lazy loading chunks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React, { Suspense } from 'react';
 import { Route, Navigate, Routes } from 'react-router-dom';
-
-import AllMovies from './pages/AllMovies';
-import MovieDetail from './pages/MovieDetail';
-import NewMovieReview from './pages/NewMovieReview';
-import NotFound from './pages/NotFound';
 import Layout from './components/layout/Layout';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { searchSuggestionsActions } from './store/slices/search-suggestions-slice';
-import AllSearchResults from './pages/AllSearchResults';
+import LoadingSpinner from './UI/LoadingSpinner';
+
+const MovieDetail = React.lazy(() => import('./pages/MovieDetail'));
+const AllSearchResults = React.lazy(() => import('./pages/AllSearchResults'));
+const AllMovies = React.lazy(() => import('./pages/AllMovies'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   const dispatch = useDispatch();
@@ -20,14 +21,21 @@ function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/movies" />} />
-        <Route path="/movies" element={<AllMovies />} />
-        <Route path="/all-movie-results/:q" element={<AllSearchResults />} />
-        <Route path="/movies/:movieId" element={<MovieDetail />} />
-        <Route path="/new-movie-review" element={<NewMovieReview />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/movies" />} />
+          <Route path="/movies" element={<AllMovies />} />
+          <Route path="/all-movie-results/:q" element={<AllSearchResults />} />
+          <Route path="/movies/:movieId" element={<MovieDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
--- src/pages/NewMovieReview.js
+++
@@ -1,5 +0,0 @@
-const NewMovieReview = () => {
-  return <h1>New movie review</h1>;
-};
-
-export default NewMovieReview;
