f6bfa8fb6dcd5738a2c918c5dcf7b861314859c8
Rino Tovar <rinotova@gmail.com>
2022-07-29 12:00:01 +0200

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
-    <div className='centered'>
-      <p>Page not found!</p>
+    <div className="centered">
+      <p className="font-teko text-3xl text-white">Page not found!</p>
     </div>
   );
 };
